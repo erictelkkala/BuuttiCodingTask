8b641063a2f2e698ecf4eeda0786c372f9f96eb6
Eric Telkkälä <eric.telkkala@gmail.com>
2022-01-24 20:34:14 +0200

    Added styles.css, added a few database functions that work. Updated README.md and dependencies as well.

---
+++ public/react_form.js
@@ -0,0 +1,37 @@
+class Form extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    //Handle the submit action
+    handleSubmit(event) {
+        event.preventDefault();
+    }
+
+    // Form
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Title:
+                    <input type="text"/>
+                </label>
+                <label>
+                    Author:
+                    <input type="text"/>
+                </label>
+                <label>
+                    Description:
+                    <input type="text"/>
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        );
+    }
+}
+
+// Render the form at the root element
+ReactDOM.render(
+    <Form />,
+    document.getElementById('root')
+);
